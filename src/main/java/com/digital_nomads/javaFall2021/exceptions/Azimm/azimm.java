f80df8b0ab0727f2f66c481cf3c9b75e522f21aa
Azim <azimtursunbekov03@gmail.com>
2022-02-22 19:25:03 +0600

    added new java class

---
+++ src/main/java/com/digital_nomads/javaFall2021/exceptions/Azimm/azimm.java
@@ -0,0 +1,8 @@
+package com.digital_nomads.javaFall2021.exceptions.Azimm;
+
+public class azimm {
+    public static void main(String[] args) {
+
+        System.out.println("this is test");
+    }
+}
